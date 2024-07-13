9a7e26d453aad739d0de6ece8e8acade1301d201
Otávio Augusto <otavioaugustobrb19@gmail.com>
2024-07-13 11:01:38 -0300

    config: adding testing routes

---
+++ src/main/routes/trips_routes.py
@@ -0,0 +1,10 @@
+from flask import jsonify, Blueprint
+
+trips_routes_bp = Blueprint("trip_routes", __name__)
+
+@trips_routes_bp.route("/trips", methods=["POST"])
+def create_trip():
+
+    return jsonify({
+        "Olá": "Mundo"
+    }), 200
